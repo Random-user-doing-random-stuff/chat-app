0697bd35cbd939460c2e1c747785208beed50299
Random-user-doing-random-stuff <25892@aevf.pt>
2024-10-30 20:31:06 +0000

    modified:   routes/index.js 	modified:   views/chatRoom.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,48 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
 
-module.exports = function(passport) {
+module.exports = function (passport) {
     const router = express.Router();
 
     // GET home page
-    router.get('/', (req, res) => {
-        res.render('index');
+    router.get("/", (req, res) => {
+        res.render("index");
     });
 
     // GET login page
-    router.get('/login', (req, res) => {
-        res.render('login');
+    router.get("/login", (req, res) => {
+        res.render("login");
     });
 
     // GET signup page
-    router.get('/signup', (req, res) => {
-        res.render('signup');
+    router.get("/signup", (req, res) => {
+        res.render("signup");
     });
 
     // POST signup
-    router.post('/signup', async (req, res) => {
+    router.post("/signup", async (req, res) => {
         const { username, password } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = new User({ username, password: hashedPassword });
-        
+
         await newUser.save();
-        res.redirect('/login');
+        res.redirect("/login");
     });
 
     // POST login
-    router.post('/login', (req, res, next) => {
-        passport.authenticate('local', {
-            successRedirect: '/chat',
-            failureRedirect: '/login'
+    router.post("/login", (req, res, next) => {
+        passport.authenticate("local", {
+            successRedirect: "/chat",
+            failureRedirect: "/login",
         })(req, res, next);
     });
 
     // GET logout
-    router.get('/logout', (req, res) => {
-        req.logout();
-        res.redirect('/');
+    router.get("/logout", (req, res) => {
+        req.logout((err) =>
+            err ? res.status(500).send("Logout failed") : res.redirect("/")
+        );
     });
 
     return router;
